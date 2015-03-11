c50dca716328303da63199c70162bafcf271438b
John Otander <johnotander@gmail.com>
2015-03-11 15:38:07 -0600

    Add get-css-classes module

---
+++ test/get-css-classes-test.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var assert = require('assert');
+var getCssClasses = require('../lib/get-css-classes');
+var postcss = require('postcss');
+
+function fixture(name) {
+    return fs.readFileSync('test/fixtures/' + name, 'utf8').trim();
+}
+
+describe('get-css-classes', function() {
+
+  it('should return a hash of the selectors', function() {
+    var css = postcss.parse(fixture('vendor.css'), { safe: true });
+    assert.deepEqual(getCssClasses(css), { '.awesome': true });
+  });
+});
