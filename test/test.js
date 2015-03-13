49e4050de7bc80c6483d1e26262386981e08669c
John Otander <johnotander@gmail.com>
2015-03-12 18:02:20 -0600

    Make the test verbose

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var immutableCss = require('..');
 describe('immutable.css', function() {
 
   it('should return false if immutability has been broken', function() {
-    assert.equal(immutableCss('test/fixtures/vendor.css', 'test/fixtures/app.css').length, 1);
+    assert.equal(immutableCss('test/fixtures/vendor.css', 'test/fixtures/app.css', { verbose: true }).length, 1);
   });
 });
