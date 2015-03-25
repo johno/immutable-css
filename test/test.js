f61c273ed4fb7de1fee8f47082a04ab6d6b5ae23
John Otander <johnotander@gmail.com>
2015-03-24 20:12:49 -0600

    Rename test description

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var immutableCss = require('..');
 
-describe('immutable.css', function() {
+describe('immutable-css', function() {
 
   it('should return false if immutability has been broken', function() {
     assert.equal(immutableCss('test/fixtures/vendor.css', 'test/fixtures/app.css', { verbose: true }).length, 5);
