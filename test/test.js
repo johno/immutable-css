1bfad71c24f9a51486e3dd9924cc4e5ec29fa2c3
John Otander <johnotander@gmail.com>
2015-04-06 18:16:27 -0600

    Don't include pseudo selectors in mutations list, closes #10

--- test/test.js
+++ test/test.js
@@ -5,5 +5,6 @@ describe('immutable-css', function() {
 
   it('should return false if immutability has been broken', function() {
     assert.equal(immutableCss('test/fixtures/vendor.css', 'test/fixtures/app.css', { verbose: true }).length, 5);
+    assert.equal(immutableCss('test/fixtures/bootstrap.css', 'test/fixtures/basscss.css', { verbose: true }).length, 13);
   });
 });
