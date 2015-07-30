6a4d4cfd002c851f5ecc4429695889070590f8d5
John Otander <johnotander@gmail.com>
2015-07-30 14:41:16 -0600

    Add support back for immutable and ignored selectors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var postcss = require('postcss')
 var getCssClasses = require('get-css-classes')
+var extendOptions = require('extend-options')
 
 module.exports = postcss.plugin('immutable-css', function (opts, cb) {
   var classMap = {}
@@ -11,28 +12,32 @@ module.exports = postcss.plugin('immutable-css', function (opts, cb) {
       opts = {}
     }
 
-    opts = opts || {}
     cb = cb || function () {}
+    opts = extendOptions({
+      immutableSelectors: [],
+      ignoredSelectors: []
+    }, opts || {})
 
     root.eachRule(function (rule) {
       rule.selectors.forEach(function (selector) {
         getCssClasses(selector).forEach(function (klass) {
           classMap[klass] = classMap[klass] || []
- 
-          if (!containsMutationFromSource(rule.source.input.from, classMap[klass])) {
-            classMap[klass].push({
-              selector: klass,
-              line: rule.source.start.line,
-              column: rule.source.start.column,
-              rule: rule
-            })
+          if (containsMutationFromSource(rule.source.input.from, classMap[klass])) {
+            return
           }
+
+          classMap[klass].push({
+            selector: klass,
+            line: rule.source.start.line,
+            column: rule.source.start.column,
+            rule: rule
+          })
         })
       })
     })
 
     Object.keys(classMap).forEach(function (mutationClass) {
-      if (classMap[mutationClass].length > 1) {
+      if (hasMutation(mutationClass, classMap, opts)) {
         result.warn(getWarningString(classMap[mutationClass]))
       }
     })
@@ -53,4 +58,9 @@ function getWarningString(mutations) {
   })
 
   return warning
+}
+
+function hasMutation(mutationClass, classMap, opts) {
+  return (classMap[mutationClass].length > 1 || opts.immutableSelectors.indexOf(mutationClass) != -1) &&
+         opts.ignoredSelectors.indexOf(mutationClass) == -1
 }
